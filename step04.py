9a669f5737b1b28ede66f5dc2b6ba1efe4ad0ea1
benkaan001 <benkaan001@gmail.com>
2022-09-19 09:41:34 +0700

    add step5

--- step04.py
+++ step04.py
@@ -1,8 +1,7 @@
-from pyspark.sql import SparkSession, functions as F
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local").appName("step3").getOrCreate()
 
-titles_df = spark.read.csv(path="./data/netflix_titles_dirty*", sep="\t", header=False)
 
 # Manipulate the csv loading method by providing a custom seperator that is not present in the dataset
 # Load the files into a DataFrame with a single column - notice that { does not exist
@@ -14,12 +13,13 @@ print(titles_single_df.count())  # 6238
 
 
 # show header
-
 print(titles_single_df.show(truncate=False))
 
 
 # print schema
 titles_single_df.printSchema()
+
+
 """
 root
  |-- _c0: string (nullable = true)
