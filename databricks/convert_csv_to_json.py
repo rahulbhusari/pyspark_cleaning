902d7c63618489cac9d7c3076c4a3ac038f67616
benkaan001 <benkaan001@gmail.com>
2022-10-17 13:49:05 -0500

    update convert_csv_to_json

--- databricks/convert_csv_to_json.py
+++ databricks/convert_csv_to_json.py
@@ -1,27 +1,24 @@
 import csv
 import json
 
-# pass in the name row that will be used as the key for the json
-key_name = "key"
-
-# determine the number of indentation for visual representation of json data
+# OPTIONAL determine the number of indentation for visual representation of json data
 indent_count = 4
 
 
 def convert_to_json(csv_path, json_path):
-    data_dict = {}
+    json_list = []
 
     with open(csv_path, encoding="utf-8") as csvf:
         csv_reader = csv.DictReader(csvf)
 
-        # convert each row into dict and append it to data_dict
-        for rows in csv_reader:
-            key = rows[key_name]
-            data_dict[key] = rows
+        # append each row in the dict from csv_reader to data_dict
+        for row in csv_reader:
+            json_list.append(row)
 
     # open a json writer and use the json.dumps() method to dump the data
     with open(json_path, "w", encoding="utf-8") as jsonf:
-        jsonf.write(json.dumps(data_dict, indent=indent_count))
+        json_string = json.dumps(json_list, indent=indent_count)
+        jsonf.write(json_string)
 
 
 # set the file paths
